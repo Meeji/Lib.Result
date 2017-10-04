f49a425a61e77901b54328b314cd605c2b851ad3
Seb <sebastian.brabiner@system1group.com>
2017-10-04 10:05:18 +0100

    Added Either method and tests

---
+++ Result.Tests/ExtensionMethods/OptionalResult.Either.Tests.cs
@@ -0,0 +1,52 @@
+﻿namespace System1Group.Lib.Result.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class OptionalResult_Either_Tests
+    {
+        [Test]
+        public void Ok_WithoutType_Success()
+        {
+            var testClass = new TestClassA();
+            var result = Result.Success<TestClassA, TestClassB>(testClass);
+
+            Assert.That(result.Either(), Is.EqualTo(testClass));
+        }
+
+        [Test]
+        public void Ok_WithoutType_Failure()
+        {
+            var testClass = new TestClassB();
+            var result = Result.Failure<TestClassA, TestClassB>(testClass);
+
+            Assert.That(result.Either(), Is.EqualTo(testClass));
+        }
+
+        [Test]
+        public void Ok_WithSameType_Success()
+        {
+            var testClass = new TestClassA();
+            var result = Result.Success<TestClassA, TestClassA>(testClass);
+
+            Assert.That(result.Either<TestClassA>(), Is.EqualTo(testClass));
+        }
+
+        [Test]
+        public void Ok_WithSameType_Failure()
+        {
+            var testClass = new TestClassA();
+            var result = Result.Failure<TestClassA, TestClassA>(testClass);
+
+            Assert.That(result.Either<TestClassA>(), Is.EqualTo(testClass));
+        }
+
+        private class TestClassA
+        {
+        }
+
+        private class TestClassB
+        {
+        }
+    }
+}
