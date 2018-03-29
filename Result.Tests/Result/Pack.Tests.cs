b1c05edcf725a613f36a999bd3fb88fc75871c0b
Seb <sebastian.brabiner@system1group.com>
2018-03-29 13:11:44 +0100

    First test

--- Result.Tests/Result/Pack.Tests.cs
+++ Result.Tests/Result/Pack.Tests.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace System1Group.Lib.Result.Tests.Result
+﻿namespace System1Group.Lib.Result.Tests
 {
-    class Pack
+    using NUnit.Framework;
+    using System1Group.Lib.Result;
+
+    [TestFixture]
+    public class Result_Pack_Tests
     {
+        private enum FailureType
+        {
+            Var
+        }
+
+        [Test]
+        public void Ok2_Success()
+        {
+            var result1 = Result.Success<string, FailureType>("some_string");
+            var result2 = Result.Success<int, FailureType>(2);
+
+            Assert.That(result1.Pack(result2).Unwrap(), Is.EqualTo(("some_string", 2)));
+        }
     }
 }
