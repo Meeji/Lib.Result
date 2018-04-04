1823c34f5539d559bfcd5cce58a9bb1f7ba66c1f
Seb <sebastian.brabiner@system1group.com>
2018-04-04 10:05:30 +0100

    More tests

--- Result.Tests/Result/Pack.Tests.cs
+++ Result.Tests/Result/Pack.Tests.cs
@@ -1,5 +1,8 @@
 ﻿namespace System1Group.Lib.Result.Tests
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using NUnit.Framework;
     using System1Group.Lib.Result;
 
@@ -8,16 +11,119 @@
     {
         private enum FailureType
         {
-            Var
+            Failed
         }
 
         [Test]
         public void Ok2_Success()
         {
-            var result1 = Result.Success<string, FailureType>("some_string");
-            var result2 = Result.Success<int, FailureType>(2);
+            var results = this.GetResults().Take(2).ToList();
 
-            Assert.That(result1.Pack(result2).Unwrap(), Is.EqualTo(("some_string", 2)));
+            Assert.That(results[0].Pack(results[1]).Unwrap(), Is.EqualTo((results[0].Unwrap(), results[1].Unwrap())));
+        }
+
+        [Test]
+        public void Ok3_Success()
+        {
+            var results = this.GetResults().Take(3).ToList();
+
+            Assert.That(results[0].Pack(results[1], results[2]).Unwrap(), Is.EqualTo((results[0].Unwrap(), results[1].Unwrap(), results[2].Unwrap())));
+        }
+
+        [Test]
+        public void Ok4_Success()
+        {
+            var results = this.GetResults().Take(4).ToList();
+
+            Assert.That(
+                results[0].Pack(results[1], results[2], results[3]).Unwrap(),
+                Is.EqualTo((results[0].Unwrap(), results[1].Unwrap(), results[2].Unwrap(), results[3].Unwrap())));
+        }
+
+        [Test]
+        public void Ok5_Success()
+        {
+            var results = this.GetResults().Take(5).ToList();
+
+            Assert.That(
+                results[0].Pack(results[1], results[2], results[3], results[4]).Unwrap(),
+                Is.EqualTo((results[0].Unwrap(), results[1].Unwrap(), results[2].Unwrap(), results[3].Unwrap(), results[4].Unwrap())));
+        }
+
+        [Test]
+        public void Ok6_Success()
+        {
+            var results = this.GetResults().Take(6).ToList();
+
+            Assert.That(
+                results[0].Pack(results[1], results[2], results[3], results[4], results[5]).Unwrap(),
+                Is.EqualTo((results[0].Unwrap(), results[1].Unwrap(), results[2].Unwrap(), results[3].Unwrap(), results[4].Unwrap(), results[5].Unwrap())));
+        }
+
+        [Test]
+        public void Ok7_Success()
+        {
+            var results = this.GetResults().Take(7).ToList();
+
+            Assert.That(
+                results[0].Pack(results[1], results[2], results[3], results[4], results[5], results[6]).Unwrap(),
+                Is.EqualTo(
+                    (results[0].Unwrap(), results[1].Unwrap(), results[2].Unwrap(), results[3].Unwrap(), results[4].Unwrap(), results[5].Unwrap(), results[6]
+                        .Unwrap())));
+        }
+
+        [Test]
+        public void Ok8_Success()
+        {
+            var results = this.GetResults().Take(8).ToList();
+
+            Assert.That(
+                results[0].Pack(results[1], results[2], results[3], results[4], results[5], results[6], results[7]).Unwrap(),
+                Is.EqualTo(
+                    (results[0].Unwrap(), results[1].Unwrap(), results[2].Unwrap(), results[3].Unwrap(), results[4].Unwrap(), results[5].Unwrap(),
+                    results[6].Unwrap(), results[7].Unwrap())));
+        }
+
+        [Test]
+        public void Ok9_Success()
+        {
+            var results = this.GetResults().Take(9).ToList();
+
+            Assert.That(
+                results[0].Pack(results[1], results[2], results[3], results[4], results[5], results[6], results[7], results[8]).Unwrap(),
+                Is.EqualTo(
+                    (results[0].Unwrap(), results[1].Unwrap(), results[2].Unwrap(), results[3].Unwrap(), results[4].Unwrap(), results[5].Unwrap(),
+                    results[6].Unwrap(), results[7].Unwrap(), results[8].Unwrap())));
+        }
+
+        [Test]
+        public void Ok10_Success()
+        {
+            var results = this.GetResults().Take(10).ToList();
+
+            Assert.That(
+                results[0].Pack(results[1], results[2], results[3], results[4], results[5], results[6], results[7], results[8], results[9]).Unwrap(),
+                Is.EqualTo(
+                    (results[0].Unwrap(), results[1].Unwrap(), results[2].Unwrap(), results[3].Unwrap(), results[4].Unwrap(), results[5].Unwrap(),
+                    results[6].Unwrap(), results[7].Unwrap(), results[8].Unwrap(), results[9].Unwrap())));
+        }
+
+        [Test]
+        public void Ok2_Failure()
+        {
+            var results = this.GetResults().Take(1).ToList();
+            var failure = Result.Failure<int, FailureType>(FailureType.Failed);
+
+            Assert.That(results[0].Pack(failure).UnwrapError(), Is.EqualTo(FailureType.Failed));
+        }
+
+        private IEnumerable<Result<int, FailureType>> GetResults()
+        {
+            var rng = new Random();
+            while (true)
+            {
+                yield return Result.Success<int, FailureType>(rng.Next());
+            }
         }
     }
 }
