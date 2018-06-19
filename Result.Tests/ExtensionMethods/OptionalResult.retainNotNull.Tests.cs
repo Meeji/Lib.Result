9a0295e90808eeddf77a98ebdaac0b1f9a762222
Unknown <sebastian.brabiner@system1group.com>
2018-06-19 14:17:11 +0100

    RetainNotNull tests

---
+++ Result.Tests/ExtensionMethods/OptionalResult.retainNotNull.Tests.cs
@@ -0,0 +1,27 @@
+﻿namespace System1Group.Lib.Result.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class OptionalResult_RetainNotNull_Tests
+    {
+        [Test]
+        public void Ok_True()
+        {
+            var result = new Success<object, string>(new { test = 6 });
+            var newResult = result.RetainNotNull("error!");
+
+            Assert.AreEqual(newResult.Unwrap(), result.Unwrap());
+        }
+
+        [Test]
+        public void Ok_SquashFailure()
+        {
+            var error = "error!";
+            var result = new Success<object, string>(null);
+            var newResult = result.RetainNotNull(error);
+
+            Assert.AreEqual(newResult.UnwrapError(), error);
+        }
+    }
+}
