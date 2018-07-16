34a01e3348b3a96c327f74e39b4bbf454d3fe178
Unknown <sebastian.brabiner@system1group.com>
2018-07-16 11:17:15 +0100

    Added async tests

---
+++ Result.Tests/AsyncResult/UnwrapAsync.Tests.cs
@@ -0,0 +1,51 @@
+﻿namespace System1Group.Lib.Result.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using Exceptions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class OptionalResult_UnwrapAsync_Tests
+    {
+        [Test]
+        public async Task Success_Ok()
+        {
+            var obj = new { field = "field" };
+            var success = new Success<object, string>(obj);
+            var async = success.ToAsyncResult();
+            var obj2 = await async.UnwrapAsync();
+
+            Assert.That(obj, Is.EqualTo(obj2));
+        }
+
+        [Test]
+        public void Failure_Ok()
+        {
+            var error = "error message";
+            var error2 = "second error message";
+            var failure = new Failure<object, string>(error);
+            var async = failure.ToAsyncResult();
+
+            var e = Assert.ThrowsAsync<InvalidUnwrapException>(async () => await async.UnwrapAsync(error2));
+            Assert.That(e.Message, Is.EqualTo(error2));
+            Assert.That(e.Result, Is.EqualTo(async));
+            Assert.That(e.Item, Is.EqualTo(error));
+            Assert.That(e.FailedUnwrapType, Is.EqualTo(InvalidUnwrapException.UnwrapType.Success));
+        }
+
+        [Test]
+        public void Failure_Ok_WithoutMessage()
+        {
+            var error = "error message";
+            var failure = new Failure<object, string>(error);
+            var async = failure.ToAsyncResult();
+
+            var e = Assert.ThrowsAsync<InvalidUnwrapException>(async () => await async.UnwrapAsync());
+            Assert.That(e.Message, Does.StartWith("Tried to unwrap"));
+            Assert.That(e.Result, Is.EqualTo(async));
+            Assert.That(e.Item, Is.EqualTo(error));
+            Assert.That(e.FailedUnwrapType, Is.EqualTo(InvalidUnwrapException.UnwrapType.Success));
+        }
+    }
+}
---
+++ Result.Tests/AsyncResult/UnwrapError.Tests.cs
@@ -0,0 +1,47 @@
+﻿namespace System1Group.Lib.Result.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using Exceptions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class OptionalResult_UnwrapErrorAsync_Tests
+    {
+        [Test]
+        public async Task Ok()
+        {
+            var error = "error";
+            var failure = new Failure<object, string>(error);
+            var async = failure.ToAsyncResult();
+            Assert.AreEqual(error, await async.UnwrapErrorAsync());
+        }
+
+        [Test]
+        public void Unwrap_Success_DefaultError()
+        {
+            var obj = new { field = "field" };
+            var success = new Success<object, string>(obj);
+            var async = success.ToAsyncResult();
+            var e = Assert.ThrowsAsync<InvalidUnwrapException>(async () => await async.UnwrapErrorAsync());
+            Assert.That(e.Message, Does.StartWith("Tried to unwrap"));
+            Assert.That(e.Result, Is.EqualTo(async));
+            Assert.That(e.Item, Is.EqualTo(obj));
+            Assert.That(e.FailedUnwrapType, Is.EqualTo(InvalidUnwrapException.UnwrapType.Failure));
+        }
+
+        [Test]
+        public void Unwrap_Success_CustomError()
+        {
+            var error = "Error text";
+            var obj = new { field = "field" };
+            var success = new Success<object, string>(obj);
+            var async = success.ToAsyncResult();
+            var e = Assert.ThrowsAsync<InvalidUnwrapException>(async () => await async.UnwrapErrorAsync(error));
+            Assert.That(e.Message, Is.EqualTo(error));
+            Assert.That(e.Result, Is.EqualTo(async));
+            Assert.That(e.Item, Is.EqualTo(obj));
+            Assert.That(e.FailedUnwrapType, Is.EqualTo(InvalidUnwrapException.UnwrapType.Failure));
+        }
+    }
+}
