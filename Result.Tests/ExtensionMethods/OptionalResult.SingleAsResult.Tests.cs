feec76fd25279530c8866b273b7be95d6cd3e511
Sebastian Brabiner <sebastian.brabiner@gmail.com>
2024-03-06 16:27:33 +0100

    SingleAsResult with predicate fix

--- Result.Tests/ExtensionMethods/OptionalResult.SingleAsResult.Tests.cs
+++ Result.Tests/ExtensionMethods/OptionalResult.SingleAsResult.Tests.cs
@@ -25,6 +25,14 @@ public class OptionalResult_SingleAsResult_Tests
     }
     
     [Test]
+    public void Ok_FirstItem_WithFunc()
+    {
+        var result = new[] { 1, 2, 3 }.SingleAsResult(i => i == 1);
+
+        Assert.That(result.Unwrap(), Is.EqualTo(1));
+    }
+    
+    [Test]
     public void Ok_WithFunc()
     {
         var result = new[] { 1, 2, 3 }.SingleAsResult(i => i == 2);
@@ -32,10 +40,21 @@ public class OptionalResult_SingleAsResult_Tests
         Assert.That(result.Unwrap(), Is.EqualTo(2));
     }
     
+    [Test]
+    public void Ok_LastItem_WithFunc()
+    {
+        var result = new[] { 1, 2, 3 }.SingleAsResult(i => i == 3);
+
+        Assert.That(result.Unwrap(), Is.EqualTo(3));
+    }
+    
     [TestCase(null, CollectionError.IsNull, TestName = "Null Collection")]
     [TestCase(new int[0], CollectionError.IsEmpty, TestName = "Empty Collection")]
     [TestCase(new[] { 1, 3, 4 }, CollectionError.NoMatchingItems, TestName = "No matching item Collection")]
     [TestCase(new[] { 1, 2, 2, 3 }, CollectionError.MultipleMatchingItems, TestName = "Multi-item Collection")]
+    [TestCase(new[] { 2, 2, 3, 4 }, CollectionError.MultipleMatchingItems, TestName = "Multi-item Collection 2")]
+    [TestCase(new[] { 2, 3, 4, 2 }, CollectionError.MultipleMatchingItems, TestName = "Multi-item Collection 3")]
+
     public void ErrorCases_WithFunc(IEnumerable<int> collection, CollectionError expectedResult)
     {
         Assert.That(collection.SingleAsResult(i => i == 2).UnwrapError(), Is.EqualTo(expectedResult));
